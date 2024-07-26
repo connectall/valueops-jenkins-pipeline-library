8705e378c642b22e9178a1c371737503eeefe4ad
Sharath <58863091+sbhaskara-ca@users.noreply.github.com>
2024-07-26 08:04:22 -0700

    removed the carrot infront of previous commit

--- vars/postCommits.groovy
+++ vars/postCommits.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]){
     sh "touch ${config.GitRepoLoc}/commit_log"
-    sh "git --git-dir=${config.GitRepoLoc}/.git log --pretty=format:'%H %ad' --date=iso ^${config.PrevSuccessBuildCommit}..${config.CurrentBuildCommit} > ${config.GitRepoLoc}/commit_log"
+    sh "git --git-dir=${config.GitRepoLoc}/.git log --pretty=format:'%H %ad' --date=iso ${config.PrevSuccessBuildCommit}..${config.CurrentBuildCommit} > ${config.GitRepoLoc}/commit_log"
     sh "echo >> ${config.GitRepoLoc}/commit_log"
     sh "cat ${config.GitRepoLoc}/commit_log"
     sh """#!/bin/bash
