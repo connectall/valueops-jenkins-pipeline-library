9544069d0dc405dde73f19a2bbbe453be061a2b4
Sharath Bhaskara <sharath.bhaskara@broadcom.com>
2024-08-29 22:32:42 -0700

    8 - check for null validation with a debug statement

--- resources/postCommitsUsingConnectALL.sh
+++ resources/postCommitsUsingConnectALL.sh
@@ -120,6 +120,7 @@ while IFS= read -r line; do
    
     # Continue if commit_id or commit_timestamp is empty
     if [ -z "$_commitId" ] || [ -z "$_commitTimestamp" ]; then
+      debug "Ignoring empty commit_id or timestamp in line: $line"
       continue
     fi
 
