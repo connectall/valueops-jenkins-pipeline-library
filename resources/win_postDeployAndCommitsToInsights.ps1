5662604d323a4a1b3c17a5919d88ae14a7945e76
Sharath Bhaskara <sharath.bhaskara@broadcom.com>
2024-11-11 19:56:45 -0800

    Troubleshooting and fixing multiple iterations for the script

--- resources/win_postDeployAndCommitsToInsights.ps1
+++ resources/win_postDeployAndCommitsToInsights.ps1
@@ -69,7 +69,9 @@ function Create-CommitLog {
         [string]$from_commit,
         [string]$to_commit
     )
-    git --git-dir="$git_repo_loc/.git" log --pretty=format:'%H %ad' --date=iso "$from_commit".."$to_commit" > $log_path
+    
+    git --git-dir="$git_repo_loc/.git" log --pretty=format:'%H %ad' --date=iso $from_commit..$to_commit > $log_path
+    #git --git-dir="./.git" log --pretty=format:'%H %ad' --date=iso "HEAD~2".."HEAD" > ./commit_log
 }
 
 # ################### ValueOps Insights functions ####################
@@ -80,9 +82,6 @@ function Query-Component {
     )
     $Resource_Endpoint="$INSIGHTS_API_URI/vsmcomponent?query=(Name%20=%20$name)&workspace=workspace/$API_WORKSPACE_OID&fetch=ObjectID"
     
-    #Info-Log "Fetching Component from Insights"
-    #Info-Log "GET URI $Resource_Endpoint"
-
     $response = Invoke-RestMethod -Uri "$Resource_Endpoint" -Headers @{
         "ZSESSIONID" = $API_KEY
     }
@@ -96,10 +95,9 @@ function Query-Component {
 
 function Get-ComponentObjectId {
     param (
-        [string]$response
+        [PSObject]$response
     )
-    #Debug-Log "Response : $response"
-    return $response | Select-Object -ExpandProperty QueryResult | Select-Object -ExpandProperty Results | Select-Object -ExpandProperty ObjectID
+    return $response | Select-Object -ExpandProperty QueryResult | select-Object -ExpandProperty Results | Select-Object -ExpandProperty ObjectID
 }
 
 
@@ -112,10 +110,16 @@ function Make-VsmDeploy {
         [string]$deploy_component_oid,
         [string]$deploy_build_id
     )
-    $json = @{
+
+    # Set time deployed if formatted_end_date is not null or not empty
+    if (-not $formatted_end_date) {
+        $formatted_end_date = $null
+    }
+
+   $json = @{
         VSMDeploy = @{
-            IsSuccessful = $deploy_is_successful
-            TimeDeployed = $formatted_end_date
+            #IsSuccessful = $deploy_is_successful
+            #TimeDeployed = $formatted_end_date
             TimeCreated = $formatted_start_date
             MainRevision = $deploy_main_revision
             Component = "vsmcomponent/$deploy_component_oid"
@@ -125,9 +129,6 @@ function Make-VsmDeploy {
 
     $Resource_Endpoint="$INSIGHTS_API_URI/vsmdeploy/create?workspace=workspace/$API_WORKSPACE_OID"
     
-    #Info-Log "Posting VSMDeploy to Insights"
-    #Info-Log "Post to URI $Resource_Endpoint with Request : $json"
-
     $response = Invoke-RestMethod -Uri "$Resource_Endpoint" -Method Post -Headers @{
         "ZSESSIONID" = $API_KEY
         "Content-Type" = "application/json"
@@ -137,15 +138,13 @@ function Make-VsmDeploy {
         Exit-WithError "Could not connect to $API_URL"
     }
 
-    #Debug-Log "Response : $response"
     return $response
 }
 
 function Get-DeployObjectId {
     param (
-        [string]$response
+        [PSObject]$response
     )
-    #Debug-Log "Response : $response"
     return $response | Select-Object -ExpandProperty CreateResult | Select-Object -ExpandProperty Object | Select-Object -ExpandProperty ObjectID
 }
 
@@ -164,8 +163,6 @@ function Make-VsmChange {
     } | ConvertTo-Json
 
     $Resource_Endpoint="$INSIGHTS_API_URI/vsmchange/create?workspace=workspace/$API_WORKSPACE_OID"
-    #Info-Log "Posting VSMChange to Insights"
-    #Info-Log "Post to URI $Resource_Endpoint with Request : $json"
 
     $response = Invoke-RestMethod -Uri "$Resource_Endpoint" -Method Post -Headers @{
         "ZSESSIONID" = $API_KEY
@@ -177,6 +174,13 @@ function Make-VsmChange {
     }
 
     return $response
+}
+
+function Get-ChangeObjectId {
+    param (
+        [PSObject]$response
+    )
+    return $response | Select-Object -ExpandProperty CreateResult | Select-Object -ExpandProperty Object | Select-Object -ExpandProperty ObjectID
 }
 
 
@@ -237,7 +241,7 @@ if (-not $?) {
 
 # Format the Build End Date if it exists
 if (-not $DEPLOY_END_TIME -or $DEPLOY_END_TIME -eq "null") {
-    $formatted_end_date = ""
+    $formatted_end_date = $null
 } else {
     $formatted_end_date = Parse-Millis $DEPLOY_END_TIME
     if (-not $?) {
@@ -250,7 +254,10 @@ $component_response = Query-Component $DEPLOY_COMPONENT_NAME
 if (-not $?) {
     Exit-WithError "Failed to query component in Insights"
 }
-Debug-Log "Response : $component_response"
+
+#Debug-Log "Response : $component_response"
+#Debug-Log "Response Type : $component_response.GetType().Name"
+#$component_response | Select-Object -ExpandProperty QueryResult | select-Object -ExpandProperty Results | Select-Object -ExpandProperty ObjectID
 
 $component_id = Get-ComponentObjectId $component_response
 if (-not $component_id) {
@@ -258,12 +265,20 @@ if (-not $component_id) {
     Exit-WithError "Failed to find component in Insights, no component id found in response."
 }
 
+Info-Log "Component found in Insights. VSMComponent.ObjectId: $($component_id)"
+
 # Make a Deploy
 $deploy_response = Make-VsmDeploy $DEPLOY_IS_SUCCESSFUL $formatted_start_date $formatted_end_date $DEPLOY_MAIN_REVISION $component_id $DEPLOY_BUILD_ID
 if (-not $?) {
     Info-Log "VSMDeploy creation failed with response:  $deploy_response"
     Exit-WithError "Failed to create deploy in Insights"
 }
+
+Debug-Log "Response : $deploy_response"
+Debug-Log "Response CreateResult : $($deploy_response.CreateResult)"
+Debug-Log "Response Error : $($deploy_response.CreateResult.Errors)"
+
+$deploy_response | Select-Object -ExpandProperty CreateResult 
 
 # Get Deploy ID
 $deploy_id = Get-DeployObjectId $deploy_response
@@ -277,7 +292,18 @@ Info-Log "Deploy created successfully. VSMDeploy.ObjectId: $deploy_id"
 
 # Create the commit log we're going to loop over
 $log_file_path = "$GIT_REPO_LOC/commit_log"
-Create-CommitLog $GIT_REPO_LOC $log_file_path $PREVIOUS_SUCCESS_BUILD_COMMIT $CURRENT_BUILD_COMMIT
+$git_commit_string = "$PREVIOUS_SUCCESS_BUILD_COMMIT..$CURRENT_BUILD_COMMIT"
+
+Debug-Log "Creating commit log for commits: $git_commit_string"
+Debug-Log "Commit contents : $(git --git-dir="$GIT_REPO_LOC/.git" log --pretty=format:'%H %ad' --date=iso $git_commit_string)"
+
+
+#Create-CommitLog $GIT_REPO_LOC $log_file_path $PREVIOUS_SUCCESS_BUILD_COMMIT $CURRENT_BUILD_COMMIT
+rm $log_file_path -ErrorAction SilentlyContinue
+git --git-dir="$GIT_REPO_LOC/.git" log --pretty=format:'%H %ad' --date=iso $git_commit_string > $log_file_path
+
+Debug-Log "Commit log created successfully"
+Debug-Log "Commit log Contents: $(Get-Content $log_file_path)"
 
 # Loop over the commit log and make VSMChanges
 Get-Content $log_file_path | ForEach-Object {
@@ -286,6 +312,7 @@ Get-Content $log_file_path | ForEach-Object {
     $commit_id = $parts[0]
     $timestamp = $parts[1..($parts.Length - 1)] -join ' '
 
+    Debug-Log "Parsing commit log line: $line"
     # Continue to the next commit if we can't parse the commit id or timestamp
     if (-not $commit_id -or -not $timestamp) {
         Error-Log "Failed to parse commit log line: $line"
@@ -296,14 +323,18 @@ Get-Content $log_file_path | ForEach-Object {
     $formatted_date = Parse-CommitLogTimestamp $timestamp
 
     # Make the VSMChange
+    Debug-Log "Creating VSMChange for commit: $commit_id with revision time: $formatted_date"
     $change_response = Make-VsmChange $commit_id $formatted_date $deploy_id
     if (-not $?) {
         Info-Log "VSMChange creation failed with response:  $change_response"
         Error-Log "Failed to create VSMChange in Insights"
     }
 
+    Debug-Log "Response : $change_response"
+    Debug-Log "Response CreateResult : $($change_response.CreateResult)"
+    Debug-Log "Response Error : $($change_response.CreateResult.Errors)"
     # Try to extract the change id
-    $change_id = Get-ObjectIdFromResponse $change_response
+    $change_id = Get-ChangeObjectId $change_response
 
     # Exit if we can't find the change id in the response (this could be for many reasons)
     if (-not $change_id) {
